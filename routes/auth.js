644b4321f1468842967db9b601b46c9969f69992
sg976 <sg9769356@gmail.com>
2023-03-11 19:35:56 +0530

    Added limit for request

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,16 @@ const authenticateUser = require('../middleware/authentication')
 const router = express.Router()
 const { register, login, updateUser } = require('../controllers/auth')
 const testUser = require('../middleware/testUser')
-router.post('/register', register)
-router.post('/login', login)
+const rateLimiter = require('express-rate-limit')
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: {
+    msg: 'Too many requests from this IP, please try again after 15 minutes',
+  },
+})
+router.post('/register', apiLimiter, register)
+router.post('/login', apiLimiter, login)
 router.patch('/updateUser', authenticateUser, testUser, updateUser)
 
 module.exports = router
