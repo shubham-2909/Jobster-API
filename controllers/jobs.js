d3c3c8678ffb3062ce713b6b813cfd37e36c0aaa
sg976 <sg9769356@gmail.com>
2023-03-11 19:20:18 +0530

    Added query parameters in search and pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,8 +3,44 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
+  const { search, status, jobType, sort } = req.query
+  const queryObject = {
+    createdBy: req.user.userId,
+  }
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' }
+  }
+  if (status && status !== 'all') {
+    queryObject.status = status
+  }
+  if (jobType && jobType !== 'all') {
+    queryObject.jobType = jobType
+  }
+
+  let result = Job.find(queryObject)
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt')
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt')
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position')
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position')
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+  const jobs = await result
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages })
 }
 const getJob = async (req, res) => {
   const {
@@ -34,7 +70,7 @@ const updateJob = async (req, res) => {
     user: { userId },
     params: { id: jobId },
   } = req
-
+  console.log(req.user)
   if (company === '' || position === '') {
     throw new BadRequestError('Company or Position fields cannot be empty')
   }
