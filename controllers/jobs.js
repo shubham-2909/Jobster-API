cda79bf5d14bc7307d21c5ff401164eea72acaf6
sg976 <sg9769356@gmail.com>
2023-03-12 14:26:11 +0530

    Setup aggregation pipeline for stats controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,7 +1,8 @@
 const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
-
+const moment = require('moment')
+const mongoose = require('mongoose')
 const getAllJobs = async (req, res) => {
   const { search, status, jobType, sort } = req.query
   const queryObject = {
@@ -70,7 +71,6 @@ const updateJob = async (req, res) => {
     user: { userId },
     params: { id: jobId },
   } = req
-  console.log(req.user)
   if (company === '' || position === '') {
     throw new BadRequestError('Company or Position fields cannot be empty')
   }
@@ -101,10 +101,55 @@ const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).send()
 }
 
+const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$status', count: { $sum: 1 } } },
+  ])
+  stats = stats.reduce((total, job) => {
+    const { _id: title, count } = job
+    total[title] = count
+    return total
+  }, {})
+  const defaultStats = {
+    declined: stats.declined || 0,
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+  }
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ])
+
+  monthlyApplications = monthlyApplications.map((item) => {
+    const {
+      _id: { year, month },
+      count,
+    } = item
+
+    const date = moment()
+      .month(month - 1)
+      .year(year)
+      .format('MMM Y')
+
+    return { date, count }
+  })
+  // console.log(monthlyApplications)
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
+}
 module.exports = {
   createJob,
   deleteJob,
   getAllJobs,
   updateJob,
   getJob,
+  showStats,
 }
