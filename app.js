82972882af599d59680bb0266108660ff13e3870
sg976 <sg9769356@gmail.com>
2023-06-10 16:28:18 +0530

    Changes

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(express.json())
 app.use(helmet())
 app.use(
   cors({
-    // origin: 'https://phoenix-jobster.netlify.app',
-    origin: 'http://localhost:3000',
+    origin: 'https://phoenix-jobster.netlify.app',
+    // origin: 'http://localhost:3000',
   })
 )
 app.use(xss())
