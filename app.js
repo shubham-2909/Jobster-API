0c2d4d01ae3c30a59a85bf8e004d9e0b2e1ae275
sg976 <sg9769356@gmail.com>
2023-06-10 15:58:50 +0530

    Allowed Frontend to Access the backend

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(helmet())
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://phoenix-jobster.netlify.app/register',
   })
 )
 app.use(xss())
